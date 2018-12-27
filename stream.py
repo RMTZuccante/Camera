e0f2839fe505886a99ce0965a973dd865558286a
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2018-12-27 23:26:50 +0100

    Pycharm is stupid

--- stream.py
+++ stream.py
@@ -1,18 +1,27 @@
-from threading import Lock
 import numpy as np
 import queue
+from cv2 import putText, FONT_HERSHEY_SIMPLEX, getTextSize
 
 
 class Stream:
     q = queue.Queue(maxsize=20)
-    lock = Lock()
 
     def __init__(self):
-        self.q.put(np.zeros((400, 400, 3), np.uint8))
+        self.addBlank()
 
     def get(self):
         return self.q.get(block=True)
 
     def add(self, frame):
-        with self.lock:
-            self.q.put(frame)
+        self.q.put(frame)
+
+    def addBlank(self):
+        self.q.put(np.zeros((400, 400, 3), np.uint8))
+
+    def addMex(self, mex, color):
+        img = np.zeros((400, 400, 3), np.uint8)
+        textsize = getTextSize(mex, FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+        textX = (img.shape[1] - textsize[0]) / 2
+        textY = (img.shape[0] + textsize[1]) / 2
+        putText(img, mex, (textX, textY), FONT_HERSHEY_SIMPLEX, 1, color, 2)
+        self.q.put(img)
