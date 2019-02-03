0a4aecf49467a8f2e6bfa6b9aa0c15259f64f0de
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2019-02-03 11:01:20 +0100

    Moved training part to python

---
+++ train/it/rtmz/train/trainSlave.py
@@ -0,0 +1,22 @@
+import base64
+import cv2
+import zmq
+
+if __name__ == "__main":
+    context = zmq.Context()
+    footage_socket = context.socket(zmq.PUB)
+    footage_socket.connect('tcp://192.168.1.7:5555')
+
+    camera = cv2.VideoCapture(0)  # init the camera
+
+    while True:
+        try:
+            grabbed, frame = camera.read()  # grab the current frame
+            encoded, buffer = cv2.imencode('.jpg', frame)
+            jpg_as_text = base64.b64encode(buffer)
+            footage_socket.send(jpg_as_text)
+
+        except KeyboardInterrupt:
+            camera.release()
+            cv2.destroyAllWindows()
+            break
