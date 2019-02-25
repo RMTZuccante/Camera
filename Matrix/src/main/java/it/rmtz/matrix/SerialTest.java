a3ee436b75cc6dae4e50f9f40e9eb8f62f016453
Marco Fincato <marcofincato777@gmail.com>
2019-02-25 15:41:13 +0100

    Moved handshake to before connection

--- Matrix/src/main/java/it/rmtz/matrix/SerialTest.java
+++ Matrix/src/main/java/it/rmtz/matrix/SerialTest.java
@@ -17,17 +17,14 @@ public class SerialTest {
             System.out.print("Port: ");
             stm = new SerialConnector(SerialPort.getCommPort(sc.nextLine()), 115200);
         }
+        System.out.println("Port opened.");
+        while(!stm.handShake()) System.out.println("Trying handshake...");
         System.out.println("Connected!");
         System.out.println(stm.getConnectionInfo());
         System.out.println();
 
         while (true) {
-            System.out.print("cmd: ");
             switch (sc.nextLine().toLowerCase()) {
-                case "handshake":
-                    System.out.print("Handshake result: ");
-                    System.out.println(stm.handShake());
-                    break;
                 case "rotate":
                     System.out.print("Angle: ");
                     stm.rotate(sc.nextInt());
