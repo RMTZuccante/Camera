9446b64da238ace48d7be5f3e7062fb0676f3742
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2019-02-26 22:17:56 +0100

    Removed old dependencies

--- Matrix/src/main/java/it/rmtz/matrix/SerialConnector.java
+++ Matrix/src/main/java/it/rmtz/matrix/SerialConnector.java
@@ -11,7 +11,7 @@ import java.nio.ByteOrder;
  * Created by Nicolò Tagliaferro
  */
 
-public class SerialConnector extends MatrixConnector {
+public class SerialConnector {
     final byte HANDSHAkE = 1, ROTATE = 2, GO = 3, GETDISTANCES = 4, GETCOLOR = 5, GETTEMPS = 6, VICTIM = 7, READY = 8;
     byte[] buffer = new byte[10];
 
@@ -19,7 +19,17 @@ public class SerialConnector extends MatrixConnector {
     int MIRROR = 1, WHITE = 0;
     int TLEFT = 0, TRIGHT = 1;
     int GOBLACK = 1, GOOBSTACLE = 2;
+    SerialPortDataListener listener = new SerialPortDataListener() {
+        @Override
+        public int getListeningEvents() {
+            return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+        }
 
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            System.out.print(new String(event.getReceivedData()));
+        }
+    };
     /* jSerialComm page http://fazecast.github.io/jSerialComm/ */
     private SerialPort stm;
 
@@ -35,18 +45,6 @@ public class SerialConnector extends MatrixConnector {
         stm.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
     }
 
-    SerialPortDataListener listener = new SerialPortDataListener() {
-        @Override
-        public int getListeningEvents() {
-            return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
-        }
-
-        @Override
-        public void serialEvent(SerialPortEvent event) {
-            System.out.print(new String(event.getReceivedData()));
-        }
-    };
-
     void enableEvents() {
         stm.addDataListener(listener);
     }
@@ -55,7 +53,6 @@ public class SerialConnector extends MatrixConnector {
         stm.removeDataListener();
     }
 
-    @Override
     /**
      * Send the HANDSHAKE command and a number, the expected response is 2 times the number
      */
@@ -68,11 +65,10 @@ public class SerialConnector extends MatrixConnector {
         int recv = stm.readBytes(buffer, 1);
         stm.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
         boolean success = recv == 1 && buffer[0] == (byte) (n * 2);
-        if(success) enableEvents();
+        if (success) enableEvents();
         return success;
     }
 
-    @Override
     void rotate(int angle) {
         boolean rot = true;
         if (angle < 0) {
@@ -85,77 +81,79 @@ public class SerialConnector extends MatrixConnector {
     void rotate(int angle, boolean right) {
         disableEvents();
         buffer[0] = ROTATE;
-        buffer[1] = (byte) (right?0:1);
+        buffer[1] = (byte) (right ? 0 : 1);
         buffer[2] = (byte) angle;
         stm.writeBytes(buffer, 3);
         stm.readBytes(buffer, 1);
         enableEvents();
     }
 
-    @Override
+
     int go() {
         disableEvents();
         buffer[0] = GO;
-        stm.writeBytes(buffer,1);
-        stm.readBytes(buffer,1);
+        stm.writeBytes(buffer, 1);
+        stm.readBytes(buffer, 1);
         enableEvents();
         return buffer[0];
     }
 
-    @Override
+
     void victim(int packets) {
         disableEvents();
         buffer[0] = VICTIM;
         buffer[1] = (byte) packets;
-        stm.writeBytes(buffer,2);
-        stm.readBytes(buffer,1);
+        stm.writeBytes(buffer, 2);
+        stm.readBytes(buffer, 1);
         enableEvents();
     }
 
-    @Override
+
     short[] getDistances() {
         disableEvents();
         int length = 2;
         int num = 5;
         buffer[0] = GETDISTANCES;
         stm.writeBytes(buffer, 1);
-        stm.readBytes(buffer, length*num);
+        stm.readBytes(buffer, length * num);
         enableEvents();
         short[] arr = new short[num];
-        for (int i = 0; i < num; i++) arr[i] = ByteBuffer.wrap(buffer, length*i,length).order(ByteOrder.LITTLE_ENDIAN).getShort();
+        for (int i = 0; i < num; i++)
+            arr[i] = ByteBuffer.wrap(buffer, length * i, length).order(ByteOrder.LITTLE_ENDIAN).getShort();
         return arr;
     }
 
-    @Override
+
     int getColor() {
         disableEvents();
         buffer[0] = GETCOLOR;
-        stm.writeBytes(buffer,1);
-        stm.readBytes(buffer,1);
+        stm.writeBytes(buffer, 1);
+        stm.readBytes(buffer, 1);
         enableEvents();
         return buffer[0];
     }
 
-    @Override
+
     float[] getTemps() {
         int length = 4;
         int num = 2;
         disableEvents();
         buffer[0] = GETTEMPS;
-        stm.writeBytes(buffer,1);
-        stm.readBytes(buffer, length*num);
+        stm.writeBytes(buffer, 1);
+        stm.readBytes(buffer, length * num);
         enableEvents();
         float[] arr = new float[num];
-        for (int i = 0; i < num; i++) arr[i] = ByteBuffer.wrap(buffer, length*i,length).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+        for (int i = 0; i < num; i++)
+            arr[i] = ByteBuffer.wrap(buffer, length * i, length).order(ByteOrder.LITTLE_ENDIAN).getFloat();
         return arr;
     }
 
     void waitForReady() {
         buffer[0] = 0;
-        while(buffer[0]!= READY) stm.readBytes(buffer,1);
+        while (buffer[0] != READY) stm.readBytes(buffer, 1);
     }
 
     String getConnectionInfo() {
-        return "Port: "+stm.getSystemPortName() + "\nBaud: "+stm.getBaudRate();
+        return "Port: " + stm.getSystemPortName() + "\nBaud: " + stm.getBaudRate();
     }
 }
